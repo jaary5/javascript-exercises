2f3e3e02f74204f636bbdc4edeeee1fafca483e3
jaary5 <mujahid.jarif770@gmail.com>
2026-01-02 21:21:32 +0600

    complete foundation/6-7

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    let res = "";
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 0; i < num; i++) {
+            res += str;
+        }
+    } 
 
+    return res;
 };
 
 // Do not edit below this line
