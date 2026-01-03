b1b2b082a5894b63672b436d5c719294ec5a6e42
jaary5 <mujahid.jarif770@gmail.com>
2026-01-03 13:24:59 +0600

    complete foundation/8-11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+ 
+    if (Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0) {
+        let sum = 0;
+        let smaller = (num1 < num2) ? num1 : num2;
+        let bigger = (num2 > num1) ? num2 : num1;
+        for (let i = smaller; i <= bigger; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    return 'ERROR'
 };
 
 // Do not edit below this line
